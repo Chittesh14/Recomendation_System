a07a9adb7f8405f8f153426dbb759588e9eb5a0c
Chittesh S <110821506+Chittesh14@users.noreply.github.com>
2022-11-07 10:59:07 +0530

    Updated UI

--- app.py
+++ app.py
@@ -14,46 +14,40 @@ if "History" not in st.session_state:
 def add_userdata(username, password):
     st.session_state.dic[username]=password
 
-#checking log in
+
 def login_user(username, password):
     for i in st.session_state.dic.keys():
         if username==i and st.session_state.dic[username]==password:
             return True
     return False
 
-#loaded files after processing in jupyter for the recomendation
 movie_lst = pickle.load(open('Movie_names_original.pkl', 'rb'))
 movie_lst = pd.DataFrame(movie_lst)
 similarity = pickle.load(open('similarity_matrix_original.pkl', 'rb'))
 images_url = pickle.load(open('images_url1.pkl', 'rb'))
 
-#baseurl for finding the shows using the given name
 baseurl='https://api.tvmaze.com/search/shows?q='
-
 def main():
-    #sidebar menu for login signin and home page for the recomendation system
     menu = ['Home', 'Login', 'Sign Up']
     choice = st.sidebar.selectbox("MENU", menu)
     if choice == 'Home':
         movie_name=st.text_input("Enter the movie name you want to search")
-        if st.button("search"):
-            #spliting the window into 3 columns 
-            col1,col2=st.columns([1,4])
-            #getting the response form the api in the json format
-            response=requests.get(baseurl+movie_name).json()
-            res=response[0]
-            #finding the image url for the medium sized image as it is dictionary accessing using keys
-            image=res['show']['image']['medium']
-            col1.image(image)
-            #similarly finding the name
-            name = res['show']['name']
-            col2.write(name)
-            #finding the runtime of rthe given movie in the tv maze api
-            runtime = res['show']['runtime']
-            col2.write('Runtime :'+str(runtime))
-            language = res['show']['language']
-            col2.write("Language: " + language)
-    #login page functions
+        try:
+            if st.button("search"):
+                col1,col2=st.columns([1,4])
+                response=requests.get(baseurl+movie_name).json()
+                res=response[0]
+                image=res['show']['image']['medium']
+                col1.image(image)
+                name = res['show']['name']
+                col2.write(name)
+                runtime = res['show']['runtime']
+                col2.write('Runtime :'+str(runtime))
+                language = res['show']['language']
+                col2.write("Language: " + language)
+        except:
+            st.warning("Please Enter a valid Entry")
+
     elif choice == 'Login':
         user_name = st.sidebar.text_input("User Name")
         password = st.sidebar.text_input('Password', type='password')
@@ -61,54 +55,63 @@ def main():
             #create_usertable()
             #result = login_user(user_name, password)
             #if password=='1234':
-            
+            #create_table()
             result=login_user(user_name,password)
             #st.write(result)
             if result:
-                #if result is true when the data is already found in the dict the request is allowed
                 st.success('Logged in as {}'.format(user_name))
                 st.title('Search for Best Movies')
-                #selecting movies form the list of movies from the plk file that has been used  and processed in th jupyter
                 selected_movie_option = st.selectbox('Choose a movie form list', movie_lst['name'].values)
-                #spliting the window into 3 equal columns
                 col1, col2, col3 = st.columns(3)
-                if col1.button('recommend'):
-                    #adding to the history if the movie is not present
-                    if selected_movie_option not in st.session_state.History:
-                        st.session_state.History.append(selected_movie_option) 
-                    #final movie list (5 movies)
-                    final_values = recomend(selected_movie_option)
-                    #finding the image url from the json data that has been returned from the api
-                    response=requests.get(baseurl+selected_movie_option).json()
-                    #getting the show values
-                    res=response[0]
-                    st.write(selected_movie_option)
-                    image = res['show']['image']['medium']
-                    st.image(image)
-                    st.title("Top Recomendations for you based on your choice")
-                    #final recomended movies
-                    for i in final_values:
-                        st.write(i)
-                        response = requests.get(baseurl + i).json()
-                        res = response[0]
+                try:
+                    if col1.button('recommend'):
+                        if selected_movie_option not in st.session_state.History:
+                            st.session_state.History.append(selected_movie_option)          #add movies to hsitory
+                        final_values = recomend(selected_movie_option)
+                        response=requests.get(baseurl+selected_movie_option).json()
+                        res=response[0]
+                        st.write(selected_movie_option)
                         image = res['show']['image']['medium']
                         st.image(image)
+                        st.title("Top Recomendations for you based on your choice")#final recomended movies
+                        image_array=[]
+                        name_array=[]
+                        for i in final_values:
+                            name_array.append((i))
+                            #st.write(i)
+                            response = requests.get(baseurl + i).json()
+                            res = response[0]
+                            image = res['show']['image']['medium']
+                            image_array.append(image)
+                            #st.image(image)
+                        c1,c2,c3 =st.columns(3)
+                        c1.write(name_array[0])
+                        c1.image(image_array[0])
+                        c2.write(name_array[1])
+                        c2.image(image_array[1])
+                        c3.write(name_array[2])
+                        c3.image(image_array[2])
+                        c4,c5,c6 = st.columns(3)
+                        c4.write(name_array[3])
+                        c4.image(image_array[3])
+                        c5.write(name_array[4])
+                        c5.image(image_array[4])
 
-                #displaying the history information
+                except:
+                    st.warning("Please select another Movie Name")
+
+
                 if col2.button('Show History'):
                     for i in st.session_state.History:
                         col2.write(i)
                     if(st.session_state.History==[]):
                         col2.write("Nothing to show up!!")
-                #clearing the history 
                 if col3.button('Clear History'):
                     st.session_state.History=[]
             else:
                 st.info("Incorrect Username/Password")
-                
-    #creating an user account 
+
     elif choice == 'Sign Up':
-        #in the main page
         st.subheader("Create New Account")
         new_user = st.text_input("UserName")
         new_password = st.text_input("Password", type='password')
@@ -130,12 +133,10 @@ def main():
 #  img_url.append(nnn[1]['medium'])
 # return  images_url
 
-#recomendation algorithm
+
 def recomend(movie):
-    # finding index so we cant lose it
-    movie_index = movie_lst[movie_lst['name'] == movie].index[0] 
-    # finding distances array
-    distances = similarity[movie_index]  
+    movie_index = movie_lst[movie_lst['name'] == movie].index[0]  # finding index so we cant lose it
+    distances = similarity[movie_index]  # finding distances array
     recomend_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
     recomended_movies_lst = []
     # recomended_movie_images=[]
